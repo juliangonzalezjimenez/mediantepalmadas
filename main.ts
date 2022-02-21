87b0e91f60ff1f116a6baed7d5c8dc7bc9aae79c
juliangonzalezjimenez <98742865+juliangonzalezjimenez@users.noreply.github.com>
2022-02-21 11:45:48 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,17 @@
-let motor = false
 input.onSound(DetectedSound.Loud, function () {
     motor = !(motor)
     if (motor) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 140)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
     } else {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+        maqueen.motorStop(maqueen.Motors.All)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
     }
 })
+let motor = false
+input.setSoundThreshold(SoundThreshold.Loud, 180)
 basic.forever(function () {
 	
 })
