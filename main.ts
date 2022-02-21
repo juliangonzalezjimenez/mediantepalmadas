c6daf8df702385827ae5f514d8f43dac428a2416
juliangonzalezjimenez <98742865+juliangonzalezjimenez@users.noreply.github.com>
2022-02-21 11:26:55 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+let motor = false
+input.onSound(DetectedSound.Loud, function () {
+    motor = !(motor)
+    if (motor) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    }
+})
 basic.forever(function () {
 	
 })
